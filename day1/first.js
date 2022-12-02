c433566422d108eba4d0e69d46b4a60dfee43448
Arnau Espin <43625217+aspnxdd@users.noreply.github.com>
2022-12-02 16:33:58 +0100

    Create first.js

---
+++ day1/first.js
@@ -0,0 +1,41 @@
+const a = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
+
+const caloriesGroup = a.split('\n');
+let groups = [[]];
+
+for (let x of caloriesGroup) {
+  if (x === '') {
+    groups.push([]);
+  } else {
+    groups.at(-1).push(parseInt(x));
+  }
+}
+
+const fat = groups
+  .map((i) => i.reduce((acc, curr) => acc + curr, 0))
+  .sort((a, b) => b - a)
+  .slice(0, 1)
+  .reduce((acc, curr) => acc + curr, 0);
+
+fat;
+
+const topThreeFat = groups
+  .map((i) => i.reduce((acc, curr) => acc + curr, 0))
+  .sort((a, b) => b - a)
+  .slice(0, 3)
+  .reduce((acc, curr) => acc + curr, 0);
+
+topThreeFat;
